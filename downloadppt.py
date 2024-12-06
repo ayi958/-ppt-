fbe1d5cd729fbaab025415a68cdd4dbc45f92f0d
ayi958 <116288804+ayi958@users.noreply.github.com>
2024-12-06 16:53:08 +0800

    Update downloadppt.py

--- downloadppt.py
+++ downloadppt.py
@@ -23,11 +23,6 @@ def download_file(download_url, filename,headers,cookies):
     else:
         print(f"下载失败，状态码：{response.status_code}")
 
-
-# 第一步：访问初始页面，获取必要的 Cookie
-session = requests.Session()
-url = "https://mooc1.chaoxing.com/mycourse/studentstudy?chapterId=921263204&courseId=233319977&clazzid=101333593&cpi=262429204&enc=b1f8060cd89557e125ca8725a8a26729&mooc2=1&openc=826e8cbb01a63f6e80be7da6652eb0ac"
-
 def get_params(url):
     # 正则表达式，匹配 'chapterId'、'courseId' 和 'clazzid'
     pattern = r"([a-zA-Z0-9]+)=(\d+)"
@@ -143,39 +138,17 @@ def get_chapterId(courseid,clazzid,headers,cookies):
 
 
 if __name__ == "__main__":
-    url = "https://mooc1.chaoxing.com/mycourse/studentstudy?chapterId=921263204&courseId=233319977&clazzid=101333593&cpi=262429204&enc=b1f8060cd89557e125ca8725a8a26729&mooc2=1&openc=826e8cbb01a63f6e80be7da6652eb0ac"
+    #随便一个章节的url
+    url = ""
     headers = {
         "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/117.0.0.0 Safari/537.36",
     }
+    #cookie值
     cookies = {
-        'k8s-ed': '1733404328.321.4018.129045',
-        'jrose': 'B7B2CDE6D15F41B3DA1A81FC67246D05.html-editor-a-1289316164-9z43t',
-        'lv': '2',
-        'fid': '35378',
-        '_uid': '236712693',
-        'uf': '94ffe74515793f3624f530c143dfa0e5bc2f4b7679f842e09fbd13d2e30b6e25c640c069eb4014d1742b6187d3a553ff273683571faa0dcd88b83130e7eb470474b646e519ab88bb65f1e82dbb36d1808487ca4cea3b44010104812c425153d878c6d095f0797980e7fafd565af53bf2',
-        '_d': '1733401454541',
-        'UID': '236712693',
-        'vc': 'A89C1549DD3569E00786B834307F1183',
-        'vc2': '27987890A0FA7FF478E001EB5EE2E793',
-        'vc3': 'aCKrZQVymMkBQwHxaGE%2BGxaWHKTSEvFwsVgkdq3A7qhIegvv4xVhadxwQ4Gubc3c2I0SgpwusYM2%2B%2BGZT1iU0rffJBeL0gPDleYQ8jNqJkAluN4WCJw%2FAyEtFx%2Bh%2FqTruFhuW4U9OHxuZG71FIOnon%2Fx7uUjV20e9x2FPNL7bbs%3Dce42200021fa560f847cd9a96eab237c',
-        'cx_p_token': '8ebae759872c2e984073598041653d94',
-        'p_auth_token': 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1aWQiOiIyMzY3MTI2OTMiLCJsb2dpblRpbWUiOjE3MzM0MDE0NTQ1NDMsImV4cCI6MTczNDAwNjI1NH0.4kO_W-VhUiKWJmGY3t0LOVeA9y9_ThMQn1uSpKpyzSk',
-        'xxtenc': '9bdc68a31c22d3175aa4758494f10c89',
-        'DSSTASH_LOG': 'C_38-UN_33335-US_236712693-T_1733401454543',
-        'createSiteSource': 'num8',
-        'wfwfid': '35378',
-        'workRoleBenchId': '0',
-        'siteType': '2',
-        'wfwEnc': '9B5DDCEF01882AD6757917BB26E1E147',
-        'tl': '1',
-        'k8s': '1733404316.787.12148.653020',
-        'route': '384a56f0aa1d1c34a64006dc82a9a2b0',
-        'source': 'num2',
-        'styleId': '',
-        'jrose': '33B40DE041F357F60E311C514CBBBDAD.mooc-490491080-56ww2'
+
     }
-    downloadpath = r"D:\桌面\zy\算法设计与实践\ppt"
+    #设置下载路径
+    downloadpath = r""
 
 
     params = get_params(url)
